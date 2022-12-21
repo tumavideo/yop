e6043ab4cba28848e35afacd1a46aafea3450b86
Anthony Sikaneta <sikaneta@gmail.com>
2022-12-21 12:39:52 +0200

    Blank the form

--- src/components/OppApplication.js
+++ src/components/OppApplication.js
@@ -1,3 +1,6 @@
-export default function OppApplication({ farmer = false, opportunity, user }) {
-  return <div className="application">shizer</div>
+import React from 'react'
+// import './OppApplication.css'
+
+export default function OppApplication() {
+  return <div className="OppApplication"></div>
 }

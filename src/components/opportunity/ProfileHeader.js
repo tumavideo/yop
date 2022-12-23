fa347033e7affb18374dade4ce51b8f42281ea4c
Anthony Sikaneta <sikaneta@gmail.com>
2022-12-23 11:37:40 +0200

    Fix profile header image

--- src/components/opportunity/ProfileHeader.js
+++ src/components/opportunity/ProfileHeader.js
@@ -23,9 +23,7 @@ export default function ProfileHeader({ company = {}, handlePress, job = {} }) {
           <img
             className="h-32 w-full object-cover lg:h-48"
             src={
-              job.companyRef.feature
-                ? urlFor(job.companyRef.feature.asset)
-                : 'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+              'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
             }
             alt="Company feature asset"
           />

bb4b024e65695698d1fb9f25985b260b2f510e16
Anthony Sikaneta <sikaneta@gmail.com>
2023-06-21 11:21:45 +0200

    Missing enableApply prop

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,5 +1,5 @@
 export const findOpportunities = (count = "5") => `{
-  "job": *[_type == "job" && !(_id in path('drafts.**'))]{_id,_createdAt,position,description,link,responsibilities,role,slug,title,companyRef->{company,bio,logo}} | order(_createdAt desc)[0..${count}],
+  "job": *[_type == "job" && !(_id in path('drafts.**'))]{_id,_createdAt,position,description,enableApply,link,responsibilities,role,slug,title,companyRef->{company,bio,logo}} | order(_createdAt desc)[0..${count}],
   "finance": *[_type == "finance" && !(_id in path('drafts.**'))]{_id,_createdAt,title,description,companyRef->{company,logo}} | order(_createdAt desc)[0..${count}],
   "career": *[_type == "career" && !(_id in path('drafts.**'))]{_id,_createdAt,companyRef->{company,logo}} | order(_createdAt desc)[0..${count}],
   "skill": *[_type == "skill" && !(_id in path('drafts.**'))]{_id,_createdAt,title,description,companyRef->{company,logo}} | order(_createdAt desc)[0..${count}]

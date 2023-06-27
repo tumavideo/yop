1dbf1ec6d0e593f08b832b04aca1b6b1effe01c4
Anthony Sikaneta <sikaneta@gmail.com>
2023-06-27 14:41:28 +0200

    Testimonies only

--- src/pages/testimony.js
+++ src/pages/testimony.js
@@ -1,17 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 import moment from "moment";
-import Link from "next/link";
 import { InfinitySpin } from "react-loader-spinner";
 
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
-import Opportunity from "@/components/Opportunity";
 import Subscribe from "@/components/layout/Subscribe";
 import Testimony from "@/components/Testimony";
 import Title from "@/components/Title";
 
-import { BANNER_URL, POST_URL, TESTIMONY_URL, PROGRAM_URL } from "../api";
+import { TESTIMONY_URL } from "../api";
 import { client } from "../lib/client";
 import { findOpportunities } from "../lib/queries";
 import { truncate } from "../utils/truncate";
@@ -49,113 +47,6 @@ export default function Home({ banner, jobs, post, testimony, program }) {
   ) : (
     <div>
       <Header />
-      {banner.length > 0 ? (
-        <section className="container mt-5">
-          <div
-            id="carouselIndicators"
-            className="carousel slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              {banner.map((item, index) => {
-                return (
-                  <button
-                    key={index}
-                    type="button"
-                    data-bs-target="#carouselIndicators"
-                    data-bs-slide-to={index}
-                    className={`${index === 0 ? "active" : ""}`}
-                    aria-current="true"
-                    aria-label={`Slide ${index}`}
-                  ></button>
-                );
-              })}
-            </div>
-            <div className="carousel-inner">
-              {banner.map((item, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={`carousel-item ${index === 0 ? "active" : ""}`}
-                  >
-                    <a href={item.uri} target="_blank">
-                      <img
-                        src={item.img}
-                        className="d-block w-100"
-                        alt="zamrise banner"
-                      />
-                    </a>
-                  </div>
-                );
-              })}
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselIndicators"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselIndicators"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-        </section>
-      ) : null}
-
-      <section id="programs">
-        <div className="container">
-          <Title text={"Govt Programs"} />
-          <div className="row">
-            {program.map((program) => (
-              <div className="col-md-4" key={program._id}>
-                <a href={`/govt/${program._id}`}>
-                  <img
-                    className="img-fluid"
-                    src={program.cover_uri}
-                    alt="govt-1"
-                  />
-                </a>
-                <a href={`${program.link_uri}`} target="_blank">
-                  <img
-                    className="mt-3 img-fluid w-50"
-                    src={program.logo_uri}
-                    alt={`pro-${program._id}`}
-                  />
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-
-      <section id="opportunities">
-        <div className="container">
-          {jobs.length > 0 && <Opportunity title="Opportunities" opps={jobs} />}
-          <div className="d-flex justify-content-center">
-            <Link
-              href="/opportunities"
-              className="button btn-primary mt-5 align-items-center _button mb-5"
-            >
-              View More
-            </Link>
-          </div>
-        </div>
-      </section>
 
       {post.length > 0 ? (
         <section id="stories">
@@ -183,33 +74,6 @@ export default function Home({ banner, jobs, post, testimony, program }) {
         </section>
       ) : null}
 
-      {post.length > 0 ? (
-        <section id="news">
-          <div className="container">
-            <Title text={"News & Updates"} />
-            <div className="row">
-              {post.slice(0, 3).map((item) => {
-                return (
-                  <div key={item._id} className="col-md-4">
-                    <a href={`/blog/${item._id}`}>
-                      <img className="img-fluid" src={item.img} alt="govt-1" />
-                    </a>
-                    <h1>{item.title}</h1>
-                    <p>{truncate(item.description, 120)}</p>
-                    <div className="d-flex">
-                      <hr />
-                      <h6 className="my-auto">
-                        {moment(item.dateCreated).format("DD MMM, YYYY")}
-                      </h6>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </section>
-      ) : null}
-
       <Subscribe />
       <Footer />
     </div>
@@ -221,12 +85,9 @@ export const getServerSideProps = async () => {
   const data = await client.fetch(query);
   const jobs = data.job;
 
-  const post = (await axios.get(POST_URL(0))).data.payload;
   const testimony = (await axios.get(TESTIMONY_URL(0))).data.payload;
-  const banner = (await axios.get(BANNER_URL)).data.payload;
-  const program = (await axios.get(PROGRAM_URL(0))).data.payload;
 
   return {
-    props: { banner, jobs, post, testimony, program },
+    props: { testimony },
   };
 };

65db7db018e8fe3e578c0963e108362070786d5d
Anthony Sikaneta <sikaneta@gmail.com>
2023-09-13 11:06:33 +0200

    Last build errors
    Remove unused APi

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -91,11 +91,7 @@ export default async function Govt() {
         ))}
         {!session ? (
           <>
-            <Filters
-              handleOnChange={() => {
-                console.log("filter");
-              }}
-            />
+            <Filters handleOnChange={false} />
             <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
               {services.map((service) => (
                 <Opportunity opp={service} />
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Header from "@/components/layout/Header";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 
+export const dynamic = "force-dynamic";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {

526957ac6faffef2394190492284830267451b3f
Anthony Sikaneta <sikaneta@gmail.com>
2023-09-20 03:33:13 +0200

    Point subscription form/button to Tally

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -108,20 +108,11 @@ export default function Footer() {
             <p className="mt-2 text-sm leading-6 text-gray-600">
               Sign up for our newsletter today!
             </p>
-            <form className="mt-6 sm:flex sm:max-w-md">
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email-address"
-                id="email-address"
-                autoComplete="email"
-                required
-                className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
-                placeholder="Enter your email"
-              />
-              <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+            <form
+              action="https://tally.so/r/wQebAg"
+              className="mt-6 sm:flex sm:max-w-md"
+            >
+              <div className="mt-4 sm:mt-0 sm:flex-shrink-0">
                 <button
                   type="submit"
                   className="flex w-full items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"

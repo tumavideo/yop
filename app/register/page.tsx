118d08c2dcecf9183950e9c4627682700cb86da4
Anthony Sikaneta <sikaneta@gmail.com>
2023-09-26 14:44:28 +0200

    Center registration form and remove background

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -15,8 +15,8 @@ export default async function Login({ searchParams: { type } }) {
 
   return (
     <>
-      <div className="bg-white flex min-h-full flex-1">
-        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 order-last">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
               <img
@@ -35,17 +35,6 @@ export default async function Login({ searchParams: { type } }) {
               </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden w-0 flex-1 lg:block">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src={
-              type === "company"
-                ? "https://images.unsplash.com/photo-1573164574572-cb89e39749b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fGFmcmljYW4lMjBjb21wYW55fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
-                : "https://images.unsplash.com/photo-1521790361543-f645cf042ec4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3269&q=80"
-            }
-            alt=""
-          />
         </div>
       </div>
     </>

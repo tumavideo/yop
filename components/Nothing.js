76da7b70be8b46d4034e7e1b1a6ebe68673a516a
Anthony Sikaneta <sikaneta@gmail.com>
2023-10-02 13:32:28 +0200

    Remove "our"

--- components/Nothing.js
+++ components/Nothing.js
@@ -1,7 +1,7 @@
 export default function Nothing({
   backgroundColor = "bg-indigo-100",
   color = "bg-indigo-600",
-  firstLine = "Ready to explore our community services?",
+  firstLine = "Ready to explore community services?",
   second = "Start your free trial today.",
 }) {
   return (

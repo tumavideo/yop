3d89efe1f5162481e90ad1089308527666cc7c23
Anthony Sikaneta <sikaneta@gmail.com>
2023-10-11 14:23:34 +0200

    Add user profile

---
+++ app/profile/update-profile-form.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { generateTimestampedFilename } from "@/utils";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { TextField } from "../auth/input";
+
+const profileSchema = z.object({
+  firstName: z.string().min(3).max(255),
+  lastName: z.string().min(3).max(255),
+  email: z.string().email(),
+  phone: z.string().min(3).max(255),
+});
+
+type ProfileValues = z.infer<typeof profileSchema>;
+
+export default async function UpdateProfileForm({ user }: { user: User }) {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const { id, email } = user;
+  const { firstName, lastName, company, phone } = user.user_metadata;
+
+  // Upload file using standard upload
+  async function uploadFile(file) {
+    const { data, error } = await supabase.storage
+      .from("resumes")
+      .upload(`${id}_${generateTimestampedFilename(file.name)}`, file);
+    if (error) {
+      // Handle error
+      console.log(error);
+    } else {
+      // Handle success
+      console.log(data);
+    }
+  }
+
+  // Create a ref to the file input element
+  const fileInputRef = useRef(null);
+
+  // Function to open the file input dialog when the button is clicked
+  const openFileInput = () => {
+    fileInputRef?.current?.click();
+  };
+
+  // Function to handle file selection
+  const handleFileSelect = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      // You can perform actions with the selected file here
+      console.log("Selected file:", selectedFile);
+      uploadFile(selectedFile);
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ProfileValues>({
+    resolver: zodResolver(profileSchema),
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit(async (formData: any) => {
+        console.log(formData, "formData");
+
+        const { firstName, lastName, email, phone, resume } = formData;
+        const { data, error } = await supabase.auth.updateUser({
+          email,
+          data: {
+            firstName,
+            lastName,
+            phone,
+            resume,
+            type: "seeker",
+          },
+        });
+        router.refresh();
+      })}
+      className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2"
+    >
+      <div className="px-4 py-6 sm:p-8">
+        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-3">
+            <TextField
+              {...register("firstName")}
+              label="First name"
+              error={errors.firstName?.message}
+              defaultValue={firstName}
+            />
+          </div>
+
+          <div className="sm:col-span-3">
+            <TextField
+              {...register("lastName")}
+              label="Last name"
+              error={errors.lastName?.message}
+              defaultValue={lastName}
+            />
+          </div>
+
+          <div className="sm:col-span-4">
+            <TextField
+              {...register("email")}
+              label="Email address"
+              error={errors.email?.message}
+              defaultValue={email}
+            />
+          </div>
+
+          <div className="sm:col-span-4">
+            <TextField
+              {...register("phone")}
+              label="Phone"
+              error={errors.phone?.message}
+              defaultValue={phone}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="px-4 py-6 sm:p-8">
+        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="col-span-full">
+            <label
+              htmlFor="cover-photo"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Resume
+            </label>
+            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+              <div id="drag-drop" className="text-center">
+                <PhotoIcon
+                  className="mx-auto h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+                <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                  <label
+                    htmlFor="file-upload"
+                    className="relative cursor-pointer rounded-md bg-white font-semibold text-red-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-600 focus-within:ring-offset-2 hover:text-red-500"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      accept=".pdf, .docx"
+                      className="sr-only"
+                      ref={fileInputRef}
+                      onChange={handleFileSelect}
+                    />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs leading-5 text-gray-600">
+                  PDF, DOCX up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}

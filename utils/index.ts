29082ed40dc2947ede93b1c458014cdb1ffe9885
Anthony Sikaneta <sikaneta@gmail.com>
2023-10-11 15:30:41 +0200

    Fix build errors

--- utils/index.ts
+++ utils/index.ts
@@ -44,3 +44,18 @@ export const compareArrays = (array1, array2) => {
 
   return resultArray;
 };
+
+export function generateTimestampedFilename(suffix) {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+
+  const timestamp = `${year}${month}${day}_${hours}${minutes}${seconds}`;
+  const filename = `${timestamp}_${suffix}`;
+
+  return filename;
+}

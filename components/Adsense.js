89fefd779d5e2ba76a182c2f856764863f5a64fd
Michael Chuba <michaelchuba@icloud.com>
2023-10-30 13:02:17 +0200

    banner udpated
    link source updated
    banner updated

--- components/Adsense.js
+++ components/Adsense.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import assets from "@/assets";
 
-const Adsense = () => {
+const Adsense = (props) => {
+
+    if(props.type === "sidebar") {
+        return (
+            <a href="https://crowdsourcecreators.com" target="_blank">
+                <img className="w-full" src={assets.sidebar.src} alt="logo"/>
+            </a>
+        );
+    }
+
+    if (props.type === "leaderboard-1") {
+        return (
+            <a href="https://crowdsourcecreators.com" target="_blank">
+                <img className="w-full" src={assets.leaderboard.src} alt="logo"/>
+            </a>
+        );
+    }
+
     return (
-        <>
-            <img className="w-auto" src={assets.adsense.src} alt="logo"/>
-        </>
+        <a href="https://crowdsourcecreators.com" target="_blank">
+            <img className="w-full" src={assets.adsense.src} alt="logo"/>
+        </a>
     );
 };
 

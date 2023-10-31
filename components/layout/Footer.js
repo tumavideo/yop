9ff337a3ea3dd6eb7bd516bf46899451ae3b28eb
Anthony Sikaneta <sikaneta@gmail.com>
2023-10-31 17:24:08 +0200

    Edit footer button

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -121,9 +121,12 @@ export default function Footer() {
             </p>
             <div className="flex mt-5">
               <div className="mt-4 sm:mt-0 sm:flex-shrink-0">
-                <a href="https://whatsapp.com/channel/0029VaDupntDjiOgFHNmuO0u" target="_blank">
+                <a
+                  href="https://whatsapp.com/channel/0029VaDupntDjiOgFHNmuO0u"
+                  target="_blank"
+                >
                   <button className="px-5 py-2 rounded-md bg-white text-sm font-semibold leading-6 text-black shadow-sm hover:text-red-600 text-center focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-                    Follow Whatsapp Channel
+                    Follow Our WhatsApp Channel
                   </button>
                 </a>
               </div>
@@ -132,20 +135,20 @@ export default function Footer() {
         </div>
       </div>
       <div
-          className="border-gray-900/10 pt-8 pb-8 sm:mt-20 lg:mt-24"
-          style={{backgroundColor: "#B80F16"}}
+        className="border-gray-900/10 pt-8 pb-8 sm:mt-20 lg:mt-24"
+        style={{ backgroundColor: "#B80F16" }}
       >
         <div className="md:flex mx-5 md:mx-auto md:items-center md:justify-between container max-w-7xl">
           <div className="space-x-6 md:order-2 flex">
             {navigation.social.map((item) => (
-                <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-white hover:text-gray-400"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon className="h-6 w-6" aria-hidden="true"/>
-                </a>
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-white hover:text-gray-400"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-white md:order-1 md:mt-0">

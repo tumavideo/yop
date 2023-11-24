cac3886f4e9b97bdb28755558e52c0e668d0014f
Anthony Sikaneta <sikaneta@gmail.com>
2023-11-24 13:26:13 +0200

    Modify apply button to take links
    Modify modal to show different messages

--- app/opportunity/[id]/page.tsx
+++ app/opportunity/[id]/page.tsx
@@ -141,16 +141,7 @@ export default async function Opportunity({
                   {opp.enableApply ? (
                     <ApplyNow opp={opp} />
                   ) : (
-                    <div className="mt-10">
-                      <a
-                        className="px-5 py-3 text-sm font-medium text-center text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                        href={opp.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Apply Now
-                      </a>
-                    </div>
+                    <ApplyNow opp={opp} link={true} />
                   )}
                 </div>
               </div>

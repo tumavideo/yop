236e0fc4d60bd15df870a4c2d91589071c46d3dc
mwila <mpnyirongo@gmail.com>
2023-11-24 20:10:04 +0200

    feat: Added pulse animation to the loading logo

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,8 +4,12 @@ export default function Loading() {
   return (
     <div className="mx-auto bg-white">
       <div className="flex h-screen items-center justify-center">
-        <div className="text-center">
-          <img src={assets.officialLogo.src} alt="logo" className="h-auto w-96" />
+        <div className="text-center animate-pulse">
+          <img
+            src={assets.officialLogo.src}
+            alt="logo"
+            className="h-auto w-96"
+          />
           <br />
         </div>
       </div>

8c11e3a1b74c2816fb11f4bb91b59dcbe39cf40c
mwila <mpnyirongo@gmail.com>
2023-12-06 16:55:08 +0200

    fix: added some loader feedback for some interation

--- app/opportunity/[id]/apply-now.tsx
+++ app/opportunity/[id]/apply-now.tsx
@@ -5,14 +5,17 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useState } from "react";
 import FailModal from "./fail-modal";
 import ThanksModal from "./thanks-modal";
+import { CurrencyPoundIcon } from "@heroicons/react/20/solid";
 
 export const ApplyNow = ({ opp, link = false }) => {
   const supabase = createClientComponentClient();
   const [submitSuccess, setSubmitSuccess] = useState(false);
   const [submitFail, setSubmitFail] = useState(false);
   const [errorMessage, setErrorMessage] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const applyForOpp = async (e) => {
+    setLoading(true);
     e.preventDefault();
 
     if (link) {
@@ -83,18 +86,37 @@ export const ApplyNow = ({ opp, link = false }) => {
     if (!error) {
       setSubmitSuccess(true);
     }
+    setLoading(false);
   };
 
   const renderButton = () => (
     <>
-      <div className="mt-10">
+      <div className="mt-10 w-20 whitespace-nowrap">
         <a
-          className="px-5 py-3 text-sm font-medium text-center text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+          className="whitespace-nowrap px-5 py-3 w-20 text-sm font-medium text-center text-white bg-red-700 rounded-lg cursor-pointer hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           onClick={applyForOpp}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apply Now
+          {loading ? (
+            <svg
+              className="animate-spin h-4 w-4 text-white inline-block align-stary"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          ) : null}
+          {!loading ? (
+            <span className="align-middle px-2">Loading...</span>
+          ) : (
+            "Apply Now"
+          )}
         </a>
       </div>
     </>

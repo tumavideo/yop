9b4bbaeca16d12fdd82dae497f7dfc0d2d5fed15
mwila <mpnyirongo@gmail.com>
2024-01-17 13:43:48 +0200

    fix: changed to use the origin instead

--- app/login/google-login-button.tsx
+++ app/login/google-login-button.tsx
@@ -20,9 +20,7 @@ function GoogleLoginButton({ children, ...props }) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${
-          process.env.NEXT_PUBLIC_AUTH_URL || location.origin
-        }/auth/callback`,
+        redirectTo: `${location.origin}/auth/callback`,
         queryParams: {
           access_type: "offline",
           prompt: "consent",
